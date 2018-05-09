1e0ff47079536dc77c73c9b3120c0019f514e753
Lukasz Kujawa <lukasz.kujawa@interia.pl>
2018-05-09 19:43:58 +0200

    change after codereview

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,9 +1,8 @@
 function getTriangleArea (a, h) {
   if (a < 0 || h < 0) {
-     console.log('Nieprawidłowe dane');
-  } else {
-    return a * h / 2;
-  }
+     return 'Nieprawidłowe dane';
+  } 
+  return a * h / 2;
 }
 console.log(getTriangleArea(10, 6));
 
